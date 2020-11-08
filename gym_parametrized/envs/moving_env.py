64938a9d0826423d6d96657dcba349bab2d11128
Thomas <hirtzt@gmail.com>
2020-11-08 11:51:38 +0100

    Edit moving_env.py

--- gym_parametrized/envs/moving_env.py
+++ gym_parametrized/envs/moving_env.py
@@ -1,27 +1,24 @@
-import gym
 from gym import spaces
 from gym.utils import seeding
 from collections import namedtuple
 from typing import Tuple
 import numpy as np
 
-Action = namedtuple('Action', ['id', 'parameter'])
+Action = namedtuple('Action', ['id', 'parameters'])
 Target = namedtuple('Target', ['x', 'y', 'radius'])
 
-
-class ActionName:
-    ACCELERATE = 0
-    BREAK = 1
-    TURN = 2
+# Action Id
+TURN = 0
+ACCELERATE = 1
+BREAK = 2
 
 
 class Agent:
-    def __init__(self, max_speed=1.0):
+    def __init__(self):
         self.x = None
         self.y = None
         self.theta = None
         self.speed = None
-        self.max_speed = max_speed
 
     def accelerate(self, value):
         self.speed += value
@@ -47,20 +44,27 @@ class MovingEnv:
     def __init__(self, seed=None):
         # Agent Parameters
         self.max_turn = 1.0
-        self.max_speed = 1.0
         self.max_acceleration = 0.5
 
         # Environment Parameters
         self.delta_t = 0.1
-        self.max_step = 500
+        self.max_step = 200
         self.field_size = 1.0
         self.target_radius = 0.1
+        self.penalty = 0.05
 
         # Initialization
-        self.step = None
+        self.current_step = None
         self.target = None
-        self.agent = Agent(self.max_speed)
+        self.agent = Agent()
         self.seed(seed)
+
+        parameters_min = np.array([-self.max_turn, 0])
+        parameters_max = np.array([+self.max_turn, self.max_acceleration])
+
+        self.action_space = spaces.Tuple((spaces.Discrete(3),
+                                          spaces.Box(parameters_min, parameters_max)))
+        self.observation_space = spaces.Box(np.ones(10), -np.ones(10))
 
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
@@ -78,35 +82,53 @@ class MovingEnv:
 
     def step(self, raw_action: Tuple[int, list]):
         action = Action(*raw_action)
-        self.step += 1
+        last_distance = self.distance
+        self.current_step += 1
 
-        if action.id == ActionName.ACCELERATE:
-            acceleration = max(min(action.parameter, self.max_speed), 0)
+        if action.id == TURN:
+            rotation = max(min(action.parameters[TURN], self.max_turn), -self.max_turn)
+            self.agent.turn(rotation)
+        elif action.id == ACCELERATE:
+            acceleration = max(min(action.parameters[ACCELERATE], self.max_acceleration), 0)
             self.agent.accelerate(acceleration)
-        elif action.id == ActionName.BREAK:
+        elif action.id == BREAK:
             self.agent.break_()
-        elif action.id == ActionName.TURN:
-            self.agent.turn(*action.parameter)
+
         self.agent.step(self.delta_t)
 
-        distance = self.get_distance(self.agent.x, self.agent.y, self.target.x, self.target.y)
-        if distance < self.target_radius and self.agent.speed == 0:
-            reward = 10
+        if self.distance < self.target_radius and self.agent.speed == 0:
+            reward = self.get_reward(last_distance, True)
             done = True
-        elif abs(self.agent.x) > self.field_size or abs(self.agent.y) > self.field_size or self.step > self.max_step:
-            reward = -10
+        elif abs(self.agent.x) > self.field_size or abs(self.agent.y) > self.field_size or self.current_step > self.max_step:
+            reward = -1
             done = True
         else:
-            reward = 0
+            reward = self.get_reward(last_distance)
             done = False
 
         return self.get_state(), reward, done, {}
 
     def get_state(self) -> list:
-        return []
+        state = [
+            self.agent.x,
+            self.agent.y,
+            self.agent.speed,
+            np.cos(self.agent.theta),
+            np.sin(self.agent.theta),
+            self.target.x,
+            self.target.y,
+            self.distance,
+            0 if self.distance > self.target_radius else 1,
+            self.current_step / self.max_step
+        ]
+        return state
 
-    def get_reward(self) -> float:
-        return 1.
+    def get_reward(self, last_distance, goal=False) -> float:
+        return self.distance - last_distance - self.penalty + (1 if goal else 0)
+
+    @property
+    def distance(self):
+        return self.get_distance(self.agent.x, self.agent.y, self.target.x, self.target.y)
 
     @staticmethod
     def get_distance(x1, y1, x2, y2):
