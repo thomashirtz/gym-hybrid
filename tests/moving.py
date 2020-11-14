cae0679f03aa6557c9295173d1eac47e7539b55f
Thomas <hirtzt@gmail.com>
2020-11-14 18:54:04 +0100

    Update moving.py

---
+++ tests/moving.py
@@ -0,0 +1,15 @@
+import gym
+import gym_hybrid
+import time
+
+env = gym.make('Moving-v0')
+env.reset()
+
+ACTION_SPACE = env.action_space[0].n
+PARAMETERS_SPACE = env.action_space[1].shape[0]
+OBSERVATION_SPACE = env.observation_space.shape[0]
+
+done = False
+while not done:
+    state, reward, done, info = env.step(env.action_space.sample())
+    print(f'State: {state} Reward: {reward} Done: {done}')
