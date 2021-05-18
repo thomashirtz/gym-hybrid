487b49d823967e5ef8af42d00971eab8ef3b1144
Thomas <hirtzt@gmail.com>
2021-05-18 12:29:25 +0200

    Update record and render tests

---
+++ tests/render.py
@@ -0,0 +1,17 @@
+import gym
+import time
+import gym_hybrid
+
+
+if __name__ == '__main__':
+    env = gym.make('Sliding-v0')
+    env.reset()
+
+    done = False
+    while not done:
+        _, _, done, _ = env.step(env.action_space.sample())
+        env.render()
+        time.sleep(0.1)
+
+    time.sleep(1)
+    env.close()
