cd05d967a9d97a131daa7d4a98eb108d2ccbfd8b
Thomas <hirtzt@gmail.com>
2023-02-18 16:49:58 +0100

    Create sliding_agent_test.py

---
+++ tests/sliding_agent_test.py
@@ -0,0 +1,21 @@
+import pytest
+from gym_hybrid.agents import SlidingAgent
+from math import pi
+
+
+def get_moving_agent(
+        break_value: float,
+        delta_t: float,
+        x: float,
+        y: float,
+        speed: float,
+        theta: float,
+        phi: float,
+) -> MovingAgent:
+    agent = SlidingAgent(break_value=break_value, delta_t=delta_t)
+    agent.x = x
+    agent.y = y
+    agent.phi = phi
+    agent.speed = speed
+    agent.theta = theta
+    return agent
